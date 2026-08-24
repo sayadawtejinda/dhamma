c8f6bb3e1d68f93784a34aa5f0ef9ebfa40267d6
sayadawtejinda <sayadawtejinda@gmail.com>
2026-08-24 13:36:44 -0700

    Delete dhamma-app directory

--- dhamma-app/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: base must match your GitHub repo name for GitHub Pages
-// Repo: https://github.com/sayadawtejinda/dhamma -> base: '/dhamma/'
-export default defineConfig({
-  plugins: [react()],
-  base: '/dhamma/',
-})
